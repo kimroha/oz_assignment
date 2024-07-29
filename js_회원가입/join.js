b66a64d42230f2a1a6fe7cec8e4a595eea39168a
깃허브 kimroha <mac@macui-MacBookPro.local>
2024-07-29 23:39:35 +0900

    회원가입

---
+++ js_회원가입/join.js
@@ -0,0 +1,35 @@
+const form = document.getElementById ("form")
+
+form.addEventListener("submit", function(event){
+    event.preventDefault() //  기존기능차단
+
+    let userId = event.target.id.value
+    let userPw1 = event.target.pw1.value
+    let userPw2 = event.target.pw2.value
+    let userName = event.target.name.value
+    let userPhone = event.target.phone.value
+    let userPosition = event.target.position.value
+    let userGender = event.target.gender.value
+    let userEmail = event.target.email.value
+    let userIntro = event.target.intro.value
+
+
+    /* console. log(userId, userPw1, userPw2, userName
+        userPhone, userPosition, userGender, userEmail, userIntro) */
+
+    if(userId.length < 6){
+        alert("아이디가 너무 짧습니다. 6자 이상 입력해주세용")
+        return
+    }
+
+    if(userPw1 !== userPw2){
+        alert("비밀번호가 일치하지 않습니돠")
+        return
+    }
+
+    document.body.innerHTML = ""
+    document.write(`<p>${userId}님 환영합니다ㅎㅎ<p>
+        <br>가입 시 입력하신 내역은 다음과 같습니다`
+    )
+    
+})
